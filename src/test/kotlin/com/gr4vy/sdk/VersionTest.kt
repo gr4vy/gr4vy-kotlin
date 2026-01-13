dcf5e097b0ea2fc1092f16bdc12f2dfedc0459e4
AJ9 <adam@aj9.co.uk>
2026-01-13 15:02:43 +0000

    V1.0.0

--- src/test/kotlin/com/gr4vy/sdk/VersionTest.kt
+++ src/test/kotlin/com/gr4vy/sdk/VersionTest.kt
@@ -26,6 +26,6 @@ class VersionTest {
 
     @Test
     fun `test version is correct`() {
-        assertEquals("1.0.0-beta.8", Version.current)
+        assertEquals("1.0.0", Version.current)
     }
 } 
