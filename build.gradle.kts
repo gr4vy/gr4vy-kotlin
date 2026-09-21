01085f13ba77e2ad6e7f7f54d4c07845cd8b7dd6
AJ9 <adam@aj9.co.uk>
2026-09-21 23:21:13 +0200

    chore(deps): consolidate September 2026 Renovate updates into v1.2.1 (#22)

--- build.gradle.kts
+++ build.gradle.kts
@@ -180,8 +180,8 @@ dependencies {
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testImplementation("org.robolectric:robolectric:4.11.1")
 
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
 
 // --------- Central Portal publishing via Vanniktech ---------
